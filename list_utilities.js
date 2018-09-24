ccc921c6758df0312892dd5ecf6d2ba3be55e59d
Freund, Robert J <robert.freund@usbank.com>
2018-09-24 09:22:26 -0500

    changed list encase from replacing 'x' to individual prepend and append textbox values

--- list_utilities.js
+++ list_utilities.js
@@ -6,39 +6,39 @@ var removeDuplicates = document.getElementById('removeDuplicates');
 var ignoreCase = document.getElementById('ignoreCase');
 var inputListSeparator = document.getElementById('inputListSeparator');
 var outputListSeparator = document.getElementById('outputListSeparator');
-var listEncase = document.getElementById('listEncase');
+var outputListItemPrepend = document.getElementById('outputListItemPrepend');
+var outputListItemAppend = document.getElementById('outputListItemAppend');
 
 Array.prototype.indexOfIgnoreCase = function(string){
     var index = -1;
     this.some(function(element, i){
         if(string.toLowerCase()!==element.toLowerCase()){ return; }
-        index = i; 
+        index = i;
         return true;
     });
     return index;
 };
-input.oninput = input.onpropertychange = getList; 
+input.oninput = input.onpropertychange = getList;
 removeDuplicates.onchange = input.onpropertychange;
 ignoreCase.onchange = input.onpropertychange;
 inputListSeparator.onchange = inputListSeparator.onkeyup = inputListSeparator.onpropertychange = input.onpropertychange;
 outputListSeparator.onchange = outputListSeparator.onkeyup = outputListSeparator.onpropertychange = input.onpropertychange;
-listEncase.onchange = listEncase.onkeyup = input.onpropertychange;
+outputListItemPrepend.onchange = outputListItemPrepend.onkeyup = outputListItemPrepend.onpropertychange = input.onpropertychange;
+outputListItemAppend.onchange = outputListItemAppend.onkeyup = outputListItemAppend.onpropertychange = input.onpropertychange;
 
-listEncase.onpropertychange = input.onpropertychange;
-
-function getList(){ 
+function getList(){
     var inputArray = input.value
     .split(inputListSeparator.value);
     inputCount.innerText = inputArray.length;
-    var outputArray = inputArray.filter(function(element, index, arr){ 
+    var outputArray = inputArray.filter(function(element, index, arr){
         input.style.height = 'auto';
         output.style.height = input.style.height;
         input.style.height = input.scrollHeight + 'px';
         output.style.height = input.style.height;
-        return (removeDuplicates.checked) ? (ignoreCase.checked) ? arr.indexOfIgnoreCase(element)===index : arr.indexOf(element)===index : true; 
+        return (removeDuplicates.checked) ? (ignoreCase.checked) ? arr.indexOfIgnoreCase(element)===index : arr.indexOf(element)===index : true;
     }).map(function(element){
-        return listEncase.value.replace('x', element); 
+        return outputListItemPrepend.value + element + outputListItemAppend.value;
     });
     outputCount.innerText = outputArray.length;
-    output.value = outputArray.join(outputListSeparator.value); 
+    output.value = outputArray.join(outputListSeparator.value);
 };
